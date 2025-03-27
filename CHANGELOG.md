e95347d886e314c0fd6693a178b3610045a67f13
smit patel <bhalodiyasmit2409@gmail.com>
2025-03-27 12:51:12 +0530

    Add initial project files, including assets, styles, and configuration

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+All notable changes to this template will be documented in this file.
+
+## v1.0.0 (2025-03-28)
+
+### Added
+
+- Initial Release
---
+++ resources/views/components/layouts/menu/vertical.blade.php
@@ -0,0 +1,43 @@
+<!-- Menu -->
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand demo">
+    <a href="{{ url('/') }}" class="app-brand-link"><x-app-logo /></a>
+  </div>
+
+  <div class="menu-inner-shadow"></div>
+
+  <ul class="menu-inner py-1">
+    <!-- Dashboards -->
+    <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
+      <a class="menu-link" href="{{ route('dashboard') }}" wire:navigate>{{ __('Dashboard') }}</a>
+    </li>
+
+    <!-- Settings -->
+    <li class="menu-item {{ request()->is('settings/*') ? 'active open' : '' }}">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-cog"></i>
+        <div class="text-truncate">{{ __('Settings') }}</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item {{ request()->routeIs('settings.profile') ? 'active' : '' }}">
+          <a class="menu-link" href="{{ route('settings.profile') }}" wire:navigate>{{ __('Profile') }}</a>
+        </li>
+        <li class="menu-item {{ request()->routeIs('settings.password') ? 'active' : '' }}">
+          <a class="menu-link" href="{{ route('settings.password') }}" wire:navigate>{{ __('Password') }}</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</aside>
+<!-- / Menu -->
+
+<script>
+  // Toggle the 'open' class when the menu-toggle is clicked
+  document.querySelectorAll('.menu-toggle').forEach(function(menuToggle) {
+    menuToggle.addEventListener('click', function() {
+      const menuItem = menuToggle.closest('.menu-item');
+      // Toggle the 'open' class on the clicked menu-item
+      menuItem.classList.toggle('open');
+    });
+  });
+</script>
